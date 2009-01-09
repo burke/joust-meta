99290ea30413c23ef8099d2c251b64816eb1aad0
Burke Libbey <burke@burkelibbey.org>
2009-01-09 00:08:52 -0600

    removed yaml stuff

--- yaml_to_el.rb
+++
@@ -1,7 +0,0 @@
-el = ""
-File.read(ARGV[0]).split('\n').each do |line|
-  puts line
-  line.scan(/(\w+): (.+)/) { |m| el << "(#{m[0]} #{m[1]})\n" }
-end
-
-File.open(ARGV[0].gsub(".yml",".el"),"w") { |f| f.puts el }
